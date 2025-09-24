7520ec600e884ad8aa2176d0571bc8339295e2e8
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-09-24 15:27:53 +0000

    Connection to created db

--- BackEnd/config.py
+++ BackEnd/config.py
@@ -1,2 +1,33 @@
 # Second file to create is a connection to the database by use of SQLAlchemy
-from os import dotenv
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv() # Load environment variables from env file
+
+database_username = os.getenv('DATABASE_USERNAME')
+database_name = os.getenv("DATABASE_NAME")
+database_password = os.getenv("DATABASE_PASSWORD")
+
+connection_string = f'mysql+mysqlconnector://{database_username}:{database_password}@localhost/{database_name}'
+
+engine = create_engine(connection_string) 
+
+try:
+    connection = engine.connect()
+    print("Database connection extablished successfully.")
+    connection.close()
+except Exception as e:
+    print(f"Error connecting to database: {e}")
+    
+DBsession = sessionmaker(bind=engine) # Create a configured "Session" class
+session = DBsession() 
+
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) 
+# def get_db():
+#     db = SessionLocal() 
+#     try:
+#         yield db
+#     finally:
+#         db.close()
