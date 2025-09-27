89d8e5ae8287d1c640e80f180978ce0d9a2e0744
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-09-27 10:55:37 +0000

    Main flask api

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,5 +1,20 @@
-# from flask import Flask, jsonify , request
-# from repositories import *
+from flask import Flask, jsonify , request
+from BackEnd.repositories import schools
+from repositories import *
 
-# app = Flask(__name__)
-# app.register_blueprint(admin_dp, url_prefix="/admin")
+app = Flask(__name__)
+app.register_blueprint(schools.school_dp)
+
+school = schools.SchoolDetails()
+@app.route("/")
+def home():
+    return "Welcome"
+@app.route("/status")
+def status():
+    return jsonify({"status": "API is running"}), 200   
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000) 
+    
