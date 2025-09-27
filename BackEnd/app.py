a46bd5466b741dbd5df6393aeaeadc448e35382f
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-09-27 11:16:58 +0000

    Api

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -3,18 +3,12 @@ from BackEnd.repositories import schools
 from repositories import *
 
 app = Flask(__name__)
-app.register_blueprint(schools.school_dp)
+app.register_blueprint(schools.school_dp, url_prefix="/schools")
 
 school = schools.SchoolDetails()
 @app.route("/")
 def home():
-    return "Welcome"
+    return "Welcome to the School Management API"
 @app.route("/status")
 def status():
     return jsonify({"status": "API is running"}), 200   
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True, port=5000) 
-    
