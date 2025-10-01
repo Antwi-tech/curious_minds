36febd80e000cdefb9c8272125aaec6ee07df5a8
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-10-01 13:14:56 +0000

    imported session from sqlalchemy

--- BackEnd/repositories/schools.py
+++ BackEnd/repositories/schools.py
@@ -1,4 +1,5 @@
 from config import SessionLocal
+from sqlalchemy.orm import Session
 from models import School
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 from typing import Optional
